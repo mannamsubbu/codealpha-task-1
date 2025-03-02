063222e22ef024a0f5aba237f8202a565fe26e02
mannamsubbu <167162262+mannamsubbu@users.noreply.github.com>
2025-03-02 12:19:24 +0530

    Add files via upload

---
+++ wordslist.py
@@ -0,0 +1,20 @@
+words = (
+    "apple", "orange", "banana", "coconut", "pineapple", "grape", "melon", "strawberry",
+    "kiwi", "blueberry", "mango", "watermelon", "lemon", "cherry", "peach", "apricot",
+    "plum", "pear", "papaya", "apricot", "fig", "date", "nectarine", "cantaloupe",
+    "pomegranate", "carrot", "broccoli", "potato", "tomato", "cucumber", "lettuce",
+    "spinach", "celery", "cauliflower", "pumpkin", "zucchini", "radish", "onion",
+    "garlic", "ginger", "beetroot", "asparagus", "brussels", "cabbage", "parsley",
+    "chili", "jalapeno", "avocado", "spinach", "basil", "mint", "oregano", "thyme",
+    "rosemary", "saffron", "cinnamon", "nutmeg", "clove", "turmeric", "cumin", "coriander",
+    "caraway", "bayleaf", "paprika", "fennel", "honey", "sugar", "salt", "butter",
+    "cheese", "cream", "milk", "yogurt", "chocolate", "coffee", "tea", "water", "juice",
+    "smoothie", "bread", "cake", "cookies", "pie", "donut", "muffin", "pasta", "pizza",
+    "burger", "fries", "sandwich", "steak", "chicken", "fish", "salmon", "tuna", "shrimp",
+    "lobster", "steak", "bacon", "sausage", "egg", "tofu", "rice", "noodles", "sushi",
+    "soup", "salad", "curry", "stew", "chili", "lasagna", "paella", "tacos", "burrito",
+    "quesadilla", "pancake", "waffle", "omelette", "croissant", "bagel", "toast",
+    "waffles", "pudding", "pie", "mousse", "cheesecake", "tiramisu", "macaron",
+    "cannoli", "baklava", "jelly", "custard", "icecream", "gelato", "sorbet", "frozen",
+    "donut", "brownie", "cupcake", "popsicle", "churros", "frenchfries", "salami", "pesto"
+)
